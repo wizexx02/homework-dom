893b246138aff45d9c911aa2ca3645f029345de4
wizexx02 <dvladvlad11@gmail.com>
2025-02-22 09:18:11 +0200

    add code

---
+++ script.js
@@ -0,0 +1,12 @@
+const button=document.querySelector("button");
+const text=document.querySelector("p");
+const firstImg=document.querySelector(".image__first");
+const list=document.querySelectorAll("li");
+const link=document.querySelector("a");
+const secondImg=document.querySelector(".image__second");
+
+secondImg.alt="photo";
+link.href="https://x.com/home?lang=en";
+list[0].textContent="80";
+firstImg.src="./img2.jpg";
+button.textContent=text.textContent;
